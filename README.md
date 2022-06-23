c848a580baf38feec849bfeaedb03a5e3fed0555
Taly <vitalik7tv@yandex.ru>
2022-06-23 19:51:17 +0300

    update script

---
+++ README.md
@@ -0,0 +1,39 @@
+# CodeX Icons
+
+Dozens of cute icons made with love by CodeX for your projects.
+
+Free to use and share.
+
+## Usage
+
+Install the package with node package manager.  
+
+```sh
+npm install @codexteam/icons
+
+yarn add @codexteam/icons
+```
+
+And import required icons.
+
+```js
+import { IconH1 } from '@codexteam/icons';
+
+console.log(IconH1);
+```
+
+You will get the line: `<svg xmlns="http://www.w3.org/2000/svg" ...`
+
+## List of icons
+
+<!-- DO NOT EDIT THE FOLLOWING SECTION MANUALLY -->
+<!-- BEGIN TABLE_OF_ICONS -->
+| Icon | Name |
+| --- | --- |
+| ![H1](./dist/icons/IconH1.svg) | `IconH1` |
+| ![H2](./dist/icons/IconH2.svg) | `IconH2` |
+| ![H3](./dist/icons/IconH3.svg) | `IconH3` |
+| ![H4](./dist/icons/IconH4.svg) | `IconH4` |
+| ![H5](./dist/icons/IconH5.svg) | `IconH5` |
+| ![H6](./dist/icons/IconH6.svg) | `IconH6` |
+<!-- END TABLE_OF_ICONS -->
--- bin/generate.js
+++ bin/generate.js
@@ -1,30 +1,59 @@
+const { optimize } = require('svgo');
 const fs = require('fs');
 
+fs.mkdirSync('./dist/icons', { recursive: true });
+
+
+const ICONS_BUNDLE_FILE = './dist/main.js';
+
 // read all files from icons directory
-const files = fs.readdirSync('./icons');
+const files = fs.readdirSync('./src/icons');
 
-// create an object for icons
-const icons = {};
+fs.writeFileSync(ICONS_BUNDLE_FILE, '');
 
-for (let file of files) {
-    // create a name of file without extension  (e.g. 'icon-name')
-    const name = file.split('.')[0];
+const TABLE_OF_ICONS = [
+    '| Icon | Name |',
+    '| --- | --- |'
+];
 
-    // push string content to object with key name
-    icons[name] = fs.readFileSync(`./icons/${file}`, 'utf8');
+for (let filename of files) {
+    const name = filename.split('.')[0];
+    const exportName = `Icon${name}`;
+
+    let iconCode = fs.readFileSync(`./src/icons/${filename}`, 'utf8');
+
+    const result = optimize(iconCode, {
+        plugins: [
+            'sortAttrs',
+            {
+                name: 'addAttributesToSVGElement',
+                params: {
+                    attributes: [
+                        {'fill': 'currentColor'}
+                    ]
+                }
+            }
+        ]
+    });
+    const optimizedSvgString = result.data;
+
+    fs.writeFileSync(`./dist/icons/${exportName}.svg`, optimizedSvgString);
+
+    fs.appendFileSync(ICONS_BUNDLE_FILE, `export const ${exportName} = '${optimizedSvgString}';\n`);
+
+    TABLE_OF_ICONS.push(`| ![${name}](./dist/icons/${exportName}.svg) | \`${exportName}\` |`);
 }
 
-// create directory dist if it doesn't exist
-if (!fs.existsSync('./dist')) {
-    fs.mkdirSync('./dist');
-}
+(function updateReadme() {
+    const readme = fs.readFileSync('./README.md', 'utf8');
+    const readmeLines = readme.split('\n');
 
-// save icons to a file
-fs.writeFileSync('./dist/icons.js', `(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-    (global.icons = factory());
-}(this, (function () {
-    'use strict';
-    return ${JSON.stringify(icons)};
-})));`);
+    const sectionStart = readmeLines.indexOf('<!-- BEGIN TABLE_OF_ICONS -->');
+    const sectionEnd = readmeLines.indexOf('<!-- END TABLE_OF_ICONS -->');
+
+    readmeLines.splice(sectionStart + 1, sectionEnd - sectionStart - 1, ...TABLE_OF_ICONS);
+
+    fs.writeFileSync('./README.md', readmeLines.join('\n'));
+})();
+
+console.log('done');
