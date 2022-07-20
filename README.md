7437e82f553c7811e2bcda644e21c9ff29839fda
Taly <vitalik7tv@yandex.ru>
2022-07-20 18:57:17 +0300

    update the code

--- README.md
+++ README.md
@@ -9,15 +9,15 @@ Free to use and share.
 Install the package with node package manager.  
 
 ```sh
-npm install @codexteam/icons
+npm install codex-icons
 
-yarn add @codexteam/icons
+yarn add codex-icons
 ```
 
 And import required icons.
 
 ```js
-import { IconH1 } from '@codexteam/icons';
+import { IconH1 } from 'codex-icons';
 
 console.log(IconH1);
 ```
@@ -30,10 +30,25 @@ You will get the line: `<svg xmlns="http://www.w3.org/2000/svg" ...`
 <!-- BEGIN TABLE_OF_ICONS -->
 | Icon | Name |
 | --- | --- |
-| ![H1](./dist/icons/IconH1.svg) | `IconH1` |
-| ![H2](./dist/icons/IconH2.svg) | `IconH2` |
-| ![H3](./dist/icons/IconH3.svg) | `IconH3` |
-| ![H4](./dist/icons/IconH4.svg) | `IconH4` |
-| ![H5](./dist/icons/IconH5.svg) | `IconH5` |
-| ![H6](./dist/icons/IconH6.svg) | `IconH6` |
+| ![IconArrowDown](./dist/icons/IconArrowDown.svg) | `IconArrowDown` |
+| ![IconArrowLeft](./dist/icons/IconArrowLeft.svg) | `IconArrowLeft` |
+| ![IconArrowRight](./dist/icons/IconArrowRight.svg) | `IconArrowRight` |
+| ![IconArrowUp](./dist/icons/IconArrowUp.svg) | `IconArrowUp` |
+| ![IconBold](./dist/icons/IconBold.svg) | `IconBold` |
+| ![IconChecklist](./dist/icons/IconChecklist.svg) | `IconChecklist` |
+| ![IconH1](./dist/icons/IconH1.svg) | `IconH1` |
+| ![IconH2](./dist/icons/IconH2.svg) | `IconH2` |
+| ![IconH3](./dist/icons/IconH3.svg) | `IconH3` |
+| ![IconH4](./dist/icons/IconH4.svg) | `IconH4` |
+| ![IconH5](./dist/icons/IconH5.svg) | `IconH5` |
+| ![IconH6](./dist/icons/IconH6.svg) | `IconH6` |
+| ![IconInlineCode](./dist/icons/IconInlineCode.svg) | `IconInlineCode` |
+| ![IconItalic](./dist/icons/IconItalic.svg) | `IconItalic` |
+| ![IconLink](./dist/icons/IconLink.svg) | `IconLink` |
+| ![IconListBullet1](./dist/icons/IconListBullet1.svg) | `IconListBullet1` |
+| ![IconListBullet](./dist/icons/IconListBullet.svg) | `IconListBullet` |
+| ![IconMenu](./dist/icons/IconMenu.svg) | `IconMenu` |
+| ![IconPlus](./dist/icons/IconPlus.svg) | `IconPlus` |
+| ![IconSearch](./dist/icons/IconSearch.svg) | `IconSearch` |
+| ![IconText](./dist/icons/IconText.svg) | `IconText` |
 <!-- END TABLE_OF_ICONS -->
--- bin/generate.js
+++ bin/generate.js
@@ -1,59 +1,241 @@
 const { optimize } = require('svgo');
+const path = require('path');
 const fs = require('fs');
 
-fs.mkdirSync('./dist/icons', { recursive: true });
+/**
+ * Module for generating icon pack file from svg icons
+ */
+class IconsGenerator {
+  constructor() {
 
+    this.ICONS_BUNDLE_FILE = './dist/main.js';
 
-const ICONS_BUNDLE_FILE = './dist/main.js';
+    /**
+     * Preparing lines for the table of icons which will be inserted to README file
+     */
+    this.TABLE_OF_ICONS = [
+      '| Icon | Name |',
+      '| --- | --- |'
+    ];
 
-// read all files from icons directory
-const files = fs.readdirSync('./src/icons');
+    /**
+     * List of input svg files
+     */
+    this.files = [];
 
-fs.writeFileSync(ICONS_BUNDLE_FILE, '');
+    this.pathToIcons = path.join(__dirname, '..', 'src', 'icons');
+  }
 
-const TABLE_OF_ICONS = [
-    '| Icon | Name |',
-    '| --- | --- |'
-];
+  run() {
+    this.recreateOutputDirectory();
+    this.createOutputScript();
 
-for (let filename of files) {
-    const name = filename.split('.')[0];
-    const exportName = `Icon${name}`;
+    this.getSvgFiles();
+    this.processSvgFiles();
 
-    let iconCode = fs.readFileSync(`./src/icons/${filename}`, 'utf8');
+    this.updateReadme();
+  }
 
-    const result = optimize(iconCode, {
-        plugins: [
-            'sortAttrs',
-            {
-                name: 'addAttributesToSVGElement',
-                params: {
-                    attributes: [
-                        {'fill': 'currentColor'}
-                    ]
-                }
-            }
-        ]
+  /**
+   * Recreating output directory
+   */
+  recreateOutputDirectory() {
+    console.log('🗄 Recreating output directory');
+
+    fs.rmSync('./dist', { recursive: true, force: true });
+    fs.mkdirSync('./dist/icons', { recursive: true });
+  }
+
+  /**
+   * Create an output script
+   */
+  createOutputScript() {
+    fs.writeFileSync(this.ICONS_BUNDLE_FILE, '');
+  }
+
+  /**
+   * Get all SVG files from icons directory
+   */
+  getSvgFiles() {
+    console.log('🔎 Getting SVG files');
+
+    this.files = fs.readdirSync(this.pathToIcons)
+      .filter(file => file.endsWith('.svg'));
+  }
+
+  /**
+   * Going through all found SVG files and process them
+   */
+  processSvgFiles() {
+    console.log('🛠 Processing SVG files');
+
+    /**
+     * Going through all SVG files
+     */
+    this.files.forEach(filename => {
+      process.stdout.write(`💎 ${filename} -> `);
+
+      /**
+       * Generate icon's export name
+       */
+      const exportName = this.generateIconExportName(filename);
+
+      console.log(`${exportName}`);
+
+      /**
+       * Read icon's code
+       */
+      const iconCode = fs.readFileSync(path.join(this.pathToIcons, filename), 'utf8');
+
+      /**
+       * Optimize icon
+       */
+      const optimizedSvgString = this.optimizeSvg(iconCode);
+
+      /**
+       * Creating icon's object to simplify processing calls
+       * @type {{svg: string, name: string}}
+       */
+      const icon = {
+        name: exportName,
+        svg: optimizedSvgString
+      };
+
+      /**
+       * Add icon to the icons bundle
+       */
+      this.appendIconToBundle(icon)
+
+      /**
+       * Save optimized file to output directory
+       */
+      this.saveOptimizedSvg(icon);
+
+      /**
+       * Push new icon to the table of icons
+       */
+      this.pushIconToTable(icon);
     });
-    const optimizedSvgString = result.data;
+  }
 
-    fs.writeFileSync(`./dist/icons/${exportName}.svg`, optimizedSvgString);
+  /**
+   * Generate icon's export name
+   *
+   * @param filename
+   *
+   * @return {string}
+   */
+  generateIconExportName(filename) {
+    /** Get the filename */
+    const name = filename
 
-    fs.appendFileSync(ICONS_BUNDLE_FILE, `export const ${exportName} = '${optimizedSvgString}';\n`);
+      /** Get text name without extension */
+      .split('.')[0]
 
-    TABLE_OF_ICONS.push(`| ![${name}](./dist/icons/${exportName}.svg) | \`${exportName}\` |`);
-}
+      /** Replace all non-alphanumeric characters with space */
+      .replace(/[^a-zA-Z0-9]/g, ' ')
 
-(function updateReadme() {
+      /** Uppercase each word and join chunks to the single word */
+      .split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('');
+
+    /**
+     * Add "Icon" prefix and return the new name
+     */
+    return `Icon${name}`;
+  }
+
+  /**
+   * Optimize SVG code
+   *
+   * @param {string} svg - SVG code
+   *
+   * @returns {string}
+   */
+  optimizeSvg(svg) {
+    /**
+     * Optimizing SVG with SVGO package
+     * Read more: https://github.com/svg/svgo#api-usage
+     */
+    const result = optimize(svg, {
+      plugins: [
+        'sortAttrs',
+        {
+          name: 'addAttributesToSVGElement',
+          params: {
+            attributes: [
+              {'fill': 'currentColor'}
+            ]
+          }
+        }
+      ]
+    });
+    return result.data;
+  }
+
+  /**
+   * Append icon to the icons bundle
+   *
+   * @param {string} name - name of the icon
+   * @param {string} svg - optimized svg code
+   */
+  appendIconToBundle({name, svg}) {
+    fs.appendFileSync(this.ICONS_BUNDLE_FILE, `export const ${name} = '${svg}';\n`);
+  }
+
+  /**
+   * Save optimized file to output directory
+   *
+   * @param {string} name - name of the icon
+   * @param {string} svg - optimized svg code
+   */
+  saveOptimizedSvg({ name , svg}) {
+    fs.writeFileSync(`./dist/icons/${name}.svg`, svg);
+  }
+
+  /**
+   * Push new icon to the table of icons for README
+   *
+   * @param {string} name - name of the icon
+   */
+  pushIconToTable({ name }) {
+    this.TABLE_OF_ICONS.push(`| ![${name}](./dist/icons/${name}.svg) | \`${name}\` |`);
+  }
+
+  /**
+   * Function for updating the icons table in README.md file
+   */
+  updateReadme() {
+    console.log('📝 Updating the README.md file');
+
+    /**
+     * Get and read README.md file
+     */
     const readme = fs.readFileSync('./README.md', 'utf8');
     const readmeLines = readme.split('\n');
 
+    /**
+     * Find start and end lines with the table of icons
+     */
     const sectionStart = readmeLines.indexOf('<!-- BEGIN TABLE_OF_ICONS -->');
     const sectionEnd = readmeLines.indexOf('<!-- END TABLE_OF_ICONS -->');
 
-    readmeLines.splice(sectionStart + 1, sectionEnd - sectionStart - 1, ...TABLE_OF_ICONS);
+    /**
+     * Insert new table of icons
+     */
+    readmeLines.splice(sectionStart + 1, sectionEnd - sectionStart - 1, ...this.TABLE_OF_ICONS);
 
+    /**
+     * Write new README.md file
+     */
     fs.writeFileSync('./README.md', readmeLines.join('\n'));
-})();
+  }
+}
 
-console.log('done');
+/**
+ * Run generator
+ */
+(new IconsGenerator()).run();
+
+
+
+
