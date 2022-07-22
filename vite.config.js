c65b76a6e70b8cdc0e80f197bf6fde88b7eabe45
ilyamore88 <moroz.ilya.olegovich@gmail.com>
2022-07-22 13:27:20 +0300

    install vite, configure vite build options

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/main.js'),
+            name: '@codexteam/icons',
+        },
+        emptyOutDir: false,
+    },
+});
