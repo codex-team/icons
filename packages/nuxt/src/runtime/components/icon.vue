e16c58c330fc6206839e6e8727066c41ade8e359
Peter Savchenko <specc.dev@gmail.com>
2023-01-22 21:37:39 +0200

    fix: nuxt ssr mode

--- packages/nuxt/src/runtime/components/icon.vue
+++ packages/nuxt/src/runtime/components/icon.vue
@@ -1,9 +1,14 @@
 <template>
-  <span v-html="icon" v-inline-svg></span>
+  <span
+    class="cdx-icon"
+    :class="class"
+    v-html="icon"
+    ref="iconWrapper"
+  ></span>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { CodexIconName } from '../../types';
 
 /**
@@ -14,30 +19,57 @@ const props = defineProps<{
    * Icon name, should be one of the names listed on https://github.com/codex-team/icons
    */
   name: CodexIconName,
+
+  /**
+   * Optional class names
+   */
+  class?: string,
+
+  /**
+   * Optional size if icon, for example: 20
+   */
+  size?:  number,
 }>()
 
 /**
  * Icon svg code to be inserted
  */
-const icon = ref('')
+const icon = ref<string>('')
 
 /**
- * Custom directive
- *
- * Used to replace <span><svg /></span> with just <svg />
- *  in construction like <span v-html="icon" v-inline-svg></span>
+ * Wrapper element reference
  */
-const vInlineSvg = {
-  mounted: (element: Element) => element.replaceWith(...element.children)
-}
+const iconWrapper = ref<HTMLElement|null>(null);
 
-try {
-  const icons = await import('@codexteam/icons')
+/**
+ * Map with all icons
+ */
+const icons = await import('@codexteam/icons')
 
-  const iconSource = icons[props.name];
+onMounted(() => {
+  try {
+    const iconSource = icons[props.name as CodexIconName];
 
-  icon.value = iconSource
-} catch {
-  console.error(`🛍 CodeX Icons: '${props.name}' doesn't exist in the pack`)
-}
+    icon.value = iconSource;
+
+    if (props.size !== undefined && iconWrapper.value !== null) {
+      iconWrapper.value.style.setProperty('--size', `${props.size}px`);
+    }
+  } catch {
+    console.error(`🛍 CodeX Icons: '${props.name}' doesn't exist in the pack`)
+  }
+})
 </script>
+
+<style>
+.cdx-icon {
+  --size: 24px;
+
+  display: inline-flex;
+}
+
+.cdx-icon svg {
+  width: var(--size);
+  height: var(--size);
+}
+</style>
