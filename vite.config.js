af65e268b0698ff76afd420e387017ca8f03ef12
dependentmadani <madani.badaoui12@gmail.com>
2024-06-30 23:59:14 +0100

    add icons types

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
         emptyOutDir: false,
     },
     plugins: [
-        dts(),
+        dts({
+            tsconfigPath: './tsconfig.json',
+          }),
     ]
 });
